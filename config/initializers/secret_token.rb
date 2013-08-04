a81102bc93cb159e0487303bfd3274d3c9a0de74
Nathan Garza (mbp) <ngarza@wphelpcenter.com>
2013-08-04 00:05:11 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'c812a5f404d1d1ef951eb5079c4501f88db0195ee30f2097f76837fc80a154bce8d1202d9fb00c1318c8d16b1057d64e2ad78bb8831a7e9221783118cbb2f3f3'
